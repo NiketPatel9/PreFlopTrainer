9a86989edb85a8ea86a8be7a2a7dc10911b9880e
Pateln1ke <patel.nike@husky.neu.edu>
2019-11-09 18:39:56 -0500

    First Commit

---
+++ src/Trainer.java
@@ -0,0 +1,8 @@
+public class Trainer {
+
+    public static void main(String args[]) {
+        Card c1 = new Card(Suit.HEART, "4");
+        System.out.println(c1.suit);
+        System.out.println(c1.value);
+    }
+}
