5cc2b491130ec198d396add6f121b9e0ab62a531
Pateln1ke <patel.nike@husky.neu.edu>
2019-12-01 14:03:21 -0500

    Added basic graphics

--- src/Trainer.java
+++ src/Trainer.java
@@ -1,10 +1,43 @@
 import cards.Card;
 import cards.Suit;
 import cards.Deck;
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
-public class Trainer {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Trainer extends JPanel {
+
+    private Point lastPoint;
+
+    public Trainer() {
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                lastPoint = new Point(e.getX(), e.getY());
+            }
+        });
+
+        addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                Graphics g = getGraphics();
+                g.drawLine(lastPoint.x, lastPoint.y, e.getX(), e.getY());
+                g.dispose();
+            }
+        });
+    }
 
     public static void main(String args[]) {
+
+        JFrame frame = new JFrame("Preflop Trainer");
+        frame.getContentPane().add(new Trainer(), BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,300);
+        frame.setVisible(true);
 
         Deck deck = new Deck();
 
