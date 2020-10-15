31bfee9b444a55637f7bda9c080641e10634020a
Pateln1ke <patel.nike@husky.neu.edu>
2020-10-15 15:53:06 -0400

    Abstraction of card drawing, will fix suits next week

--- src/Trainer.java
+++ src/Trainer.java
@@ -16,79 +16,41 @@ public class Trainer extends JPanel {
 	private static final int CARD_Y = 20;
 	private static final int CARD_WIDTH = 25;
 	private static final int CARD_HEIGHT = 75;
+	private static List<Player> players = new ArrayList<Player>();
+
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
 		Font f = new Font("Card Values", Font.PLAIN, 48);
-		g.setFont(f);
-
-		g.drawRect(750, 600, 80, 120);
-		g.drawRect(850, 600, 80, 120);
-		g.drawString("A", 775, 675);
-		g.drawString("A", 875, 675);
-
-
-		g.drawRect(400, 450, 80, 120);
-		g.drawRect(500, 450, 80, 120);
-		g.drawString("K", 425, 525);
-		g.drawString("K", 525, 525);
-
-		g.drawRect(400, 250, 80, 120);
-		g.drawRect(500, 250, 80, 120);
-		g.drawString("Q", 425, 325);
-		g.drawString("Q", 525, 325);
-
-
-		g.drawRect(750, 100, 80, 120);
-		g.drawRect(850, 100, 80, 120);
-		g.drawString("J", 775, 175);
-		g.drawString("J", 875, 175);
-
-		g.drawRect(1100, 250, 80, 120);
-		g.drawRect(1200, 250, 80, 120);
-		g.drawString("10", 1125, 325);
-		g.drawString("10", 1225, 325);
-
-		g.drawRect(1100, 450, 80, 120);
-		g.drawRect(1200, 450, 80, 120);
-		g.drawString("9", 1125, 525);
-		g.drawString("9", 1225, 525);
-
 		Font f2 = new Font("Card Suits", Font.PLAIN, 36);
-		g.setFont(f2);
 
-		g.drawString("♠", 750, 630);
-		g.drawString("♠", 800, 710);
-		g.drawString("♣", 850, 630);
-		g.drawString("♣", 900, 710);
+		for (Player p : players) {
+			System.out.println(p.getName() + " " + p.getHand() + " " + p.getPosition() + p.getRelevantCoordinates());
 
-		g.drawString("♥", 400, 480);
-		g.drawString("♥", 450, 560);
-		g.drawString("♦", 500, 480);
-		g.drawString("♦", 550, 560);
+			g.setFont(f);
+			g.drawRect(p.getRelevantCoordinates().get("CardOneTopLeft").x,
+					p.getRelevantCoordinates().get("CardOneTopLeft").y,
+					80, 120);
+			g.drawRect(p.getRelevantCoordinates().get("CardTwoTopLeft").x,
+					p.getRelevantCoordinates().get("CardTwoTopLeft").y,
+					80, 120);
+			g.drawString("A", p.getRelevantCoordinates().get("CardOneValue").x,
+					p.getRelevantCoordinates().get("CardOneValue").y);
+			g.drawString("A", p.getRelevantCoordinates().get("CardTwoValue").x,
+					p.getRelevantCoordinates().get("CardTwoValue").y);
 
-		g.drawString("♠", 400, 280);
-		g.drawString("♠", 450, 360);
-		g.drawString("♥", 500, 280);
-		g.drawString("♥", 550, 360);
-
-		g.drawString("♦", 750, 130);
-		g.drawString("♦", 800, 210);
-		g.drawString("♣", 850, 130);
-		g.drawString("♣", 900, 210);
-
-		g.drawString("♥", 1100, 280);
-		g.drawString("♥", 1150, 360);
-		g.drawString("♣", 1200, 280);
-		g.drawString("♣", 1250, 360);
-
-		g.drawString("♠", 1100, 480);
-		g.drawString("♠", 1150, 560);
-		g.drawString("♦", 1200, 480);
-		g.drawString("♦", 1250, 560);
-
+			g.setFont(f2);
+			g.drawString("♠", p.getRelevantCoordinates().get("CardOneTopSuit").x,
+					p.getRelevantCoordinates().get("CardOneTopSuit").y);
+			g.drawString("♠", p.getRelevantCoordinates().get("CardOneBottomSuit").x,
+					p.getRelevantCoordinates().get("CardOneBottomSuit").y);
+			g.drawString("♠", p.getRelevantCoordinates().get("CardTwoTopSuit").x,
+					p.getRelevantCoordinates().get("CardTwoTopSuit").y);
+			g.drawString("♠", p.getRelevantCoordinates().get("CardTwoBottomSuit").x,
+					p.getRelevantCoordinates().get("CardTwoBottomSuit").y);
+		}
 	}
 
 
@@ -114,7 +76,6 @@ public class Trainer extends JPanel {
         deck.shuffle();
 
         int numPlayers = 6;
-        List<Player> players = new ArrayList<Player>();
 
         for (int i = 0; i < numPlayers; i++) {
             ArrayList<Card> holeCards = new ArrayList<Card>();
@@ -124,10 +85,6 @@ public class Trainer extends JPanel {
             players.add(player);
         }
 
-        for (Player p : players) {
-            System.out.println(p.getName() + " " + p.getHand() + " " + p.getPosition());
-        }
-
 	    SwingUtilities.invokeLater(new Runnable() {
 		    @Override
 		    public void run() {
@@ -135,7 +92,6 @@ public class Trainer extends JPanel {
 		    }
 	    });
 
-//        System.out.println(deck.showCards());
     }
 
 }
--- src/player/Player.java
+++ src/player/Player.java
@@ -2,8 +2,8 @@ package player;
 
 import cards.Card;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.awt.Point;
 
 public class Player {
 
@@ -20,4 +20,55 @@ public class Player {
 	public String getName() { return this.name; }
 	public List<Card> getHand() { return this.hand; }
 	public Pos getPosition() { return this.position; }
+
+	public Map<String, Point> getRelevantCoordinates() {
+		ArrayList<String> relevantCoords = new ArrayList<String>(Arrays.asList("CardOneTopLeft", "CardTwoTopLeft",
+				"CardOneValue", "CardTwoValue", "CardOneTopSuit", "CardOneBottomSuit",
+				"CardTwoTopSuit", "CardTwoBottomSuit"));
+
+		ArrayList<Point> adjustments = new ArrayList<Point>(Arrays.asList(
+				new Point(0, 0),
+				new Point(100, 0),
+				new Point(25, 75),
+				new Point(125, 75),
+				new Point(0, 30),
+				new Point(50, 110),
+				new Point(100, 30),
+				new Point(150, 110)
+		));
+
+		Map<String, Point> nameToCoordinate = new HashMap<String, Point>();
+		Point basePoint;
+		switch (this.position) {
+			case BTN:
+				basePoint = new Point(750, 600);
+				break;
+			case SB:
+				basePoint = new Point(400, 450);
+				break;
+			case BB:
+				basePoint = new Point(400, 250);
+				break;
+			case UTG:
+				basePoint = new Point(750, 100);
+				break;
+			case HJ:
+				basePoint = new Point(1100, 250);
+				break;
+			case CO:
+				basePoint = new Point(1100, 450);
+				break;
+			default:
+				basePoint = new Point(0, 0);
+				break;
+		}
+
+		for(int i = 0; i < relevantCoords.size(); i++) {
+			int adjustedX = basePoint.x + adjustments.get(i).x;
+			int adjustedY = basePoint.y + adjustments.get(i).y;
+			nameToCoordinate.put(relevantCoords.get(i), new Point(adjustedX, adjustedY));
+		}
+		return nameToCoordinate;
+	}
+
 }
