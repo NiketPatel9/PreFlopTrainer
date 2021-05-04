d38686e62bfe6342d2a7895b989f7a3097242962
Pateln1ke <patel.nike@husky.neu.edu>
2021-05-03 22:50:09 -0400

    Tested royalflush and straight flush, will implement full house next

--- src/util/HandComparator.java
+++ src/util/HandComparator.java
@@ -6,9 +6,7 @@ import cards.Suit;
 import player.Player;
 import util.CombinationCalc;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class HandComparator {
 	public void determineWinner(List<Player> players, Set<Card> combo) {
@@ -32,6 +30,23 @@ public class HandComparator {
 		Set<Card> bestHand;
 		Hand hand;
 
+		Card c1 = new Card(Suit.HEART, "10");
+		Card c2 = new Card(Suit.HEART, "K");
+		Card c3 = new Card(Suit.HEART, "Q");
+		Card c4 = new Card(Suit.HEART, "J");
+		Card c5 = new Card(Suit.HEART, "9");
+
+		Set<Card> testHand = new HashSet<Card>();
+		testHand.add(c1);
+		testHand.add(c2);
+		testHand.add(c3);
+		testHand.add(c4);
+		testHand.add(c5);
+
+		fiveCardCombos = new ArrayList<Set<Card>>();
+		fiveCardCombos.add(testHand);
+
+
 		for (Set<Card> combo : fiveCardCombos) {
 
 			List<Suit> suits = new ArrayList<>();
@@ -42,9 +57,21 @@ public class HandComparator {
 				suits.add(card.getSuit());
 			}
 
+			System.out.println(suits);
+			System.out.println(values);
+
 			if (this.checkRoyalFlush(suits, values)) {
 				bestHand = combo;
 				hand = Hand.ROYAL;
+			}
+			else if (this.checkStraightFlush(suits, values)) {
+				bestHand = combo;
+				hand = Hand.STRAIGHTFLUSH;
+				System.out.println("Do i get here?");
+			}
+			else if (this.checkFullHouse(suits, values)) {
+				bestHand = combo;
+				hand = Hand.BOAT;
 			}
 			else {
 				bestHand = combo;
@@ -70,6 +97,10 @@ public class HandComparator {
 	}
 
 	private boolean checkFlush(List<Suit> suits, List<String> values) {
+		return true;
+	}
+
+	private boolean checkFullHouse(List<Suit> suits, List<String> values) {
 		return true;
 	}
 
